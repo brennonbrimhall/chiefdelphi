896366e1e8d3846e571e3c05acf2806c0b515554
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-03-15 14:54:48 -0600

    Store in SQLite database.

--- main.go
+++ main.go
@@ -2,35 +2,66 @@ package main
 
 import (
 	"chiefdelphi/chiefdelphi"
+	"database/sql"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func populateUsers() {
+func initalizeDB(file string) *sql.DB {
+	database, _ := sql.Open("sqlite3", file)
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER UNIQUE NOT NULL, username TEXT NOT NULL, team INT)")
+	statement.Exec()
+
+	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS topics (id INTEGER UNIQUE NOT NULL, title TEXT NOT NULL)")
+	statement.Exec()
+
+	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS posts (id INTEGER UNIQUE NOT NULL, userid INTEGER NOT NULL, topicid INTEGER NOT NULL, timestamp TIMESTAMP NOT NULL, body TEXT NOT NULL)")
+	statement.Exec()
+
+	return database
+}
+
+func populateUsers(database *sql.DB) {
+	statement, _ := database.Prepare("INSERT INTO users (id, username, team) VALUES (?, ?, ?)")
+
 	users := chiefdelphi.GetUsers()
 
 	for user := range users {
 		fmt.Printf("%d,%s,%d\n", user.ID, user.Username, user.Team)
+		statement.Exec(user.ID, user.Username, user.Team)
 	}
 }
 
-func populateTopics() {
+func populateTopics(database *sql.DB) {
+	statement, _ := database.Prepare("INSERT INTO topics (id, title) VALUES (?, ?)")
+
 	topics := chiefdelphi.GetTopics()
 
 	for topic := range topics {
 		fmt.Printf("%d,%s\n", topic.ID, topic.Title)
+		statement.Exec(topic.ID, topic.Title)
 	}
 }
 
-func populatePosts() {
+func populatePosts(database *sql.DB) {
+	statement, _ := database.Prepare("INSERT INTO posts (id, userid, topicid, timestamp, body) VALUES (?, ?, ?, ?)")
+
 	posts := chiefdelphi.GetPosts()
 
 	for post := range posts {
 		fmt.Printf("%d,%d,%s,%s\n", post.ID, post.UserID, post.Timestamp.String(), post.Body)
+		statement.Exec(post.ID, post.UserID, post.Timestamp, post.Body)
 	}
 }
 
 func main() {
-	populateUsers()
-	populateTopics()
-	populatePosts()
+	database := initalizeDB("./chiefdelphi.db")
+
+	populateUsers(database)
+	populateTopics(database)
+	populatePosts(database)
+
+	database.Close()
 }
